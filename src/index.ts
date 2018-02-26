41d593f3a74c4f50f41882f36aa5e6e3b9c782cd
kenfreee <denisbelgo@gmail.com>
2018-02-27 00:22:25 +0300

    refact async

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,24 @@
-import * as typeorm from 'typeorm';
+import {createConnection, Connection} from 'typeorm';
 import * as TelegramBot from 'node-telegram-bot-api';
 import token from './config';
 import routerCommands from './router-commands';
 
-console.info('Connecting to database...');
+(async () => {
+	console.info('Connecting to database...');
 
-typeorm.createConnection().then((connection: typeorm.Connection) => {
-	console.info('Connected!');
+	try{
+		const connection: Connection = await createConnection();
+	
+		console.info('Connected!');
 
-	const bot: TelegramBot = new TelegramBot(token, {polling: true});
+		const bot: TelegramBot = new TelegramBot(token, {polling: true});
+	
+		bot.onText(/\/([^\s]+)\s+(.+)/, (msg: any, match: string[]): void => {
+			console.info('Received a message from the user.');
 
-	bot.onText(/\/([^\s]+)\s+(.+)/, (msg: any, match: string[]): void => {
-		console.info('Received a message from the user.');
-
-		routerCommands(msg, match, bot);
-	});
-}).catch(error => {
-	console.error("Error: ", error);
-});
+			routerCommands(msg, match, bot);
+		});
+	} catch(error) {
+		console.error("Error: ", error.message);
+	}
+})(); 
--- src/storage/index.ts
+++ src/storage/index.ts
@@ -1,21 +1,21 @@
-import * as typeorm from 'typeorm';
+import {getConnection, Connection} from 'typeorm';
 import User from "./entity/User";
 import Record from "./entity/Record";
 
-const add = (msg: any, text: string, callback): void => {
-	const connection: typeorm.Connection = typeorm.getConnection();
+const add = async (msg: any, text: string, callback) => {
+	const connection: Connection = getConnection();
 	const {id, first_name: firstname, last_name: lastname} = msg.from;
 	const user = new User(id, firstname, lastname);
 	
-	connection.manager
-		.save(user)
-		.then(user => {
-        	callback(`User named ${user.firstname} was successfully added.`);
-        	console.info(`User named ${user.firstname} was successfully added.`);
-    	}).catch(error => {
-    		callback(`User named ${user.firstname} already exists.`);
-    		console.error("Error: ", `User named ${user.firstname} already exists.`);
-    	});
+	try {
+		await connection.manager.save(user);
+
+		console.info(`User named ${user.firstname} was successfully added.`);
+		callback(`User named ${user.firstname} was successfully added.`);
+	} catch (error) {
+		callback(`User named ${user.firstname} already exists.`);
+		console.error("Error: ", `User named ${user.firstname} already exists.`);
+	}
 };
 
 export default {
